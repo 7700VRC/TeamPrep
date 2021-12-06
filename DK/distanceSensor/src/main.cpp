6cc1413c2ad35c2937600e04e7472d369d2bdc45
George Kirkman <dk@rollingrobots.com>
2021-12-06 09:10:36 -0800

    DK distance sensor

---
+++ DK/distanceSensor/src/main.cpp
@@ -0,0 +1,112 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// LDrive               motor_group   1, 2            
+// RDrive               motor_group   9, 10           
+// LDist                distance      11              
+// CDist                distance      12              
+// RDist                distance      13              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+void drive(int lspeed, int rspeed, int wt) {
+  LDrive.spin(forward, lspeed, percent);
+  RDrive.spin(forward, rspeed, percent);
+  wait(wt, msec);
+}
+
+void seekObject() {
+  float d = 3.0;
+  float kp=2.0;
+  float L = 0.0;
+  float C = 0.0;
+  float R = 0.0;
+  float lsp=0.0;
+  float rsp=0.0;
+  while (true) {
+    L = LDist.objectDistance(inches);
+    C = CDist.objectDistance(inches);
+    R = RDist.objectDistance(inches);
+    Brain.Screen.printAt(1, 40, "L,C,R  %.1f   %.1f   %.1f", L, C, R);
+    lsp=kp*(R-C);
+    rsp=kp*(L-C);
+    drive(lsp, rsp, 10);
+  }
+}
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  float L = 0.0;
+  float C = 0.0;
+  float R = 0.0;
+  seekObject();
+  while (1) {
+    L = LDist.objectDistance(inches);
+    C = CDist.objectDistance(inches);
+    R = RDist.objectDistance(inches);
+    Brain.Screen.printAt(1, 40, "L,C,R  %.1f   %.1f   %.1f", L, C, R);
+    wait(20, msec);
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
