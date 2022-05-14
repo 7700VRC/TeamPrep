c27c3bc42c0186aabc1b2ec6956db7426f320f69
George Kirkman <dk@rollingrobots.com>
2022-05-13 21:56:34 -0700

    start flywheel code

---
+++ Flywheel/src/main.cpp
@@ -0,0 +1,124 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       DK 7700                                                   */
+/*    Created:      May 13, 2022                                              */
+/*    Description:  Speed Control for 2 motor Flywheel testing                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// F1                   motor         1               
+// F2                   motor         2               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+double OldError=0.0;
+double TBHval=0.0;
+double FWDrive=0.0;
+
+void spinFlywheel(double speed)
+{
+  speed=speed*120;  //speed is in percentage so convert to mV 100% = 12000 mV
+  F1.spin(forward, speed, voltageUnits::mV);
+  F2.spin(forward, speed, voltageUnits::mV);
+}
+
+void controlFlywheelSpeed(double target)
+{
+  double kI=.0025;
+  double speed = F1.velocity(percent);
+  double error=target-speed;
+  double fwDrive=FWDrive+kI*error;
+  //Keep drive between 0 to 100%
+  if(fwDrive>100) fwDrive=100;
+  if(fwDrive<0) fwDrive=0;
+  //Check for zero crossing
+  if (error*OldError<0)
+  {
+    fwDrive=0.5*(fwDrive+TBHval);
+    TBHval=fwDrive;
+  }
+  spinFlywheel(fwDrive);
+
+  FWDrive=fwDrive;
+  OldError=error;
+
+}
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
