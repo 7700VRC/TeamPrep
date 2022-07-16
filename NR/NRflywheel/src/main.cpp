538ff3ab2c22deccae0c488a9e945c0d20d525f8
It'sAlex <86859304+EcstaticPilot@users.noreply.github.com>
2022-07-15 17:37:24 -0700

    added brake

--- NR/NRflywheel/src/main.cpp
+++ NR/NRflywheel/src/main.cpp
@@ -2,25 +2,24 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       DK 7700                                                   */
+/*    Author:       NR 7700                                                   */
 /*    Created:      May 13, 2022                                              */
-/*    Description:  Speed Control for 2 motor Flywheel testing                */
-/*     many update by hc team  6/5/22 */
+/*    Description:  code of Nikhil Ramanuja                                   */
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// F1                   motor         12              
-// F2                   motor         21              
-// Injector             digital_out   A               
-// LF                   motor         18              
-// LB                   motor         19              
-// RF                   motor         16              
-// RB                   motor         17              
-// Intake1              motor         9               
-// Intake2              motor         10              
+// Controller1          controller
+// F1                   motor         12
+// F2                   motor         21
+// Injector             digital_out   A
+// LF                   motor         18
+// LB                   motor         19
+// RF                   motor         16
+// RB                   motor         17
+// Intake1              motor         9
+// Intake2              motor         10
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -36,12 +35,13 @@ double OldError = 0.0;
 double TBHval = 0.0;
 double FWDrive = 0.0;
 
-void controlFlywheel1(double target){
-    double speed = F1.velocity(percent);
+void controlFlywheel1(double target) {
+  double speed = F1.velocity(percent);
 
-spinFlywheel((target-speed)+target);  Brain.Screen.printAt(180, 40, "fwdrive %.1f  ", target);
+  spinFlywheel((target - speed) + target);
+  Brain.Screen.printAt(180, 40, "fwdrive %.1f  ", target);
 
- Brain.Screen.printAt(1, 40, " speed = %.2f ", speed);
+  Brain.Screen.printAt(1, 40, " speed = %.2f ", speed);
 }
 
 void controlFlywheelSpeed(double target) {
@@ -89,30 +89,24 @@ void flywheelMonitor() {
   Brain.Screen.printAt(1, 100, "Battery Capacity  = %.1f      ", b);
 }
 void pistonToggle() {
- 
-    Injector.set(true);
+
+  Injector.set(true);
   wait(.5, sec);
   Injector.set(false);
-    
-  
 }
 void pistonToggleReady() {
-Brain.Screen.drawRectangle(120, 190, 60, 60, orange);
- waitUntil(F1.velocity(percent) < targetSpeed+.5 && F1.velocity(percent) > targetSpeed-.5);
-    Brain.Screen.drawRectangle(120, 190, 60, 60, black);
+  Brain.Screen.drawRectangle(120, 190, 60, 60, orange);
+  waitUntil(F1.velocity(percent) < targetSpeed + .5 &&
+            F1.velocity(percent) > targetSpeed - .5);
+  Brain.Screen.drawRectangle(120, 190, 60, 60, black);
 
-    Injector.set(true);
+  Injector.set(true);
   wait(.5, sec);
   Injector.set(false);
   Brain.Screen.drawRectangle(120, 190, 60, 60, black);
-
-    
-  
 }
-bool intakeOn=false;
-void toggleIntake(){
-  intakeOn=!intakeOn;
-}
+bool intakeOn = false;
+void toggleIntake() { intakeOn = !intakeOn; }
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -128,7 +122,6 @@ void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
   Brain.Screen.drawRectangle(120, 190, 60, 60, black);
-
 }
 
 /*---------------------------------------------------------------------------*/
@@ -155,21 +148,21 @@ void autonomous(void) {}
 
 void usercontrol(void) {
 
-  
   bool alg = true;
   bool flag = true;
   while (true) {
-    
-    int ax1=Controller1.Axis1.position();
-    int ax2=Controller1.Axis2.position();
-    int ax4=Controller1.Axis4.position();
-    
+
+    int ax1 = Controller1.Axis1.position();
+    int ax2 = Controller1.Axis2.position();
+    int ax4 = Controller1.Axis4.position();
+
     if (Controller1.ButtonA.pressing())
       targetSpeed = 0;
 
-    if (Controller1.ButtonB.pressing()){
-     intakeOn=!intakeOn;
-     wait(100, msec);}
+    if (Controller1.ButtonB.pressing()) {
+      intakeOn = !intakeOn;
+      wait(100, msec);
+    }
     if (Controller1.ButtonY.pressing())
       targetSpeed = 34;
     if (Controller1.ButtonX.pressing())
@@ -191,37 +184,36 @@ void usercontrol(void) {
       controlFlywheel1(targetSpeed);
       Brain.Screen.printAt(1, 120, "not controlled     ");
     }
-    if (Controller1.ButtonR1.pressing() && targetSpeed < 100) {targetSpeed++;
-      wait(100, msec);}
-    if (Controller1.ButtonL1.pressing() && targetSpeed > 0) {
-      targetSpeed--;
-      wait(100, msec);
-    }
-    if(intakeOn){
+
+    if (intakeOn) {
       Intake1.spin(forward, 100, percent);
       Intake2.spin(forward, 100, percent);
 
+    } else {
+      Intake1.stop(coast);
+      Intake1.stop(coast);
     }
-    else {
-    Intake1.stop(coast);
-    Intake1.stop(coast);
-    }
-    if (F1.velocity(percent) < targetSpeed+1 && F1.velocity(percent) > targetSpeed-1) {
+    if (F1.velocity(percent) < targetSpeed + 1 &&
+        F1.velocity(percent) > targetSpeed - 1) {
       Brain.Screen.drawRectangle(60, 190, 60, 60, green);
-     
+
     } else {
       Brain.Screen.drawRectangle(60, 190, 60, 60, red);
     }
     flywheelMonitor();
-    //x drive code
-    LF.spin(forward, ax2+ax1+.5*ax4, percent);
-    RF.spin(forward, -ax2+ax1+.5*ax4, percent);
-    LB.spin(forward, -ax2-ax1+.5*ax4, percent);
-    RB.spin(forward, ax2-ax1+.5*ax4, percent);
-    
+    // x drive code
+    LF.spin(forward, ax2 + ax1 + .5 * ax4, percent);
+    RF.spin(forward, -ax2 + ax1 + .5 * ax4, percent);
+    LB.spin(forward, -ax2 - ax1 + .5 * ax4, percent);
+    RB.spin(forward, ax2 - ax1 + .5 * ax4, percent);
+    if (ax1 && ax2 && ax2 == 0) {
+      LF.stop(hold);
+      RF.stop(hold);
+      LB.stop(hold);
+      RB.stop(hold);
+    }
   }
 }
-
 //
 // Main will set up the competition functions and callbacks.
 //
@@ -229,8 +221,8 @@ int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
-  Controller1.ButtonL2.pressed(pistonToggle);
-   Controller1.ButtonRight.pressed(pistonToggleReady);
+  Controller1.ButtonL1.pressed(pistonToggle);
+  Controller1.ButtonRight.pressed(pistonToggleReady);
   Controller1.ButtonL2.pressed(toggleIntake);
   // Run the pre-autonomous function.
   pre_auton();
