c53c5918e2a13afb68cb157060d83ccb3f61d986
George Kirkman <gkirkman@rollingrobots.com>
2023-04-07 06:42:31 -0700

    vision 1

--- MeineKleineproject DK/include/robot-config.h
+++ MeineKleineproject DK/include/robot-config.h
@@ -2,6 +2,8 @@ using namespace vex;
 
 extern brain Brain;
 
+using signature = vision::signature;
+
 // VEXcode devices
 extern controller Controller1;
 extern motor RMmotor;
@@ -14,6 +16,14 @@ extern inertial Gyro;
 extern optical Color;
 extern distance Dist;
 extern motor Intake;
+extern signature Vision21__SNAIL;
+extern signature Vision21__SENSOR;
+extern signature Vision21__SIG_3;
+extern signature Vision21__SIG_4;
+extern signature Vision21__SIG_5;
+extern signature Vision21__SIG_6;
+extern signature Vision21__SIG_7;
+extern vision Vision21;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- MeineKleineproject DK/src/robot-config.cpp
+++ MeineKleineproject DK/src/robot-config.cpp
@@ -19,6 +19,11 @@ inertial Gyro = inertial(PORT3);
 optical Color = optical(PORT4);
 distance Dist = distance(PORT5);
 motor Intake = motor(PORT13, ratio6_1, false);
+/*vex-vision-config:begin*/
+signature Vision21__SNAIL = signature (1, -5827, -5397, -5612, -3855, -3413, -3634, 2.5, 0);
+signature Vision21__SENSOR = signature (2, 4249, 5249, 4749, 917, 1555, 1236, 3, 0);
+vision Vision21 = vision (PORT21, 50, Vision21__SNAIL, Vision21__SENSOR);
+/*vex-vision-config:end*/
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
