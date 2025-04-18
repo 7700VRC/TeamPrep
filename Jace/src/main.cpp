db8137572560022ad63522e1b48deafd8f4de475
7700R <127266781+7700BVRC@users.noreply.github.com>
2025-04-17 18:10:52 -0700

    jace code

---
+++ Jace/src/main.cpp
@@ -0,0 +1,128 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       student                                                   */
+/*    Created:      4/17/2025, 4:14:18 PM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+brain Brain;
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*
+     
+/*  not every time that the robot is disabled*/
+
+//return type: int, float,void                      */
+void drawOnScreen  () {
+  Brain.Screen.printAt (240, 135, "MIDDLE")
+  
+Brain.Screen.printAt (120,80,"Left Top corner");
+Brain.Screen.printAt (100,200,  "bottom Left Corner");
+  
+  
+  
+  }
+
+int drawShapes () {
+  Brain.Screen.setPenColor(white);
+Brain.Screen.setFillColor(yellow);
+  Brain.Screen.drawRectangle(250, 100, 50, 50);
+  
+  
+
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+/*not every time the robot is disabled
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+   drawOnScreen();
+  wait (1,sec);
+Brain.Screen.clearScreen();
+drawShapes();
+
+}
+
+
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
