43dc9c97819690834b69a6c93dde49ec040a072c
7700R <94393717+7700picasso@users.noreply.github.com>
2025-04-24 17:55:10 -0700

    ooga booga
    goofy ahh

--- 7700Dunkleoosteous/src/main.cpp
+++ 7700Dunkleoosteous/src/main.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       student2                                                  */
+/*    Author:       TTHEGOAT                                                  */
 /*    Created:      4/17/2025, 4:38:55 PM                                     */
 /*    Description:  V5 project                                                */
 /*                                                                            */
@@ -14,7 +14,15 @@ using namespace vex;
 // A global instance of competition
 competition Competition;
 brain Brain;
+motor LF (PORT6, ratio6_1, false);
+motor LB (PORT11, ratio6_1, false);
+motor RF (PORT9, ratio6_1, true);
+motor RB (PORT20, ratio6_1, true);
+
 // define your global instances of motors and other devices here
+float dia = 3.25;
+float gr = 0.6;
+float pi = M_PI;
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -36,15 +44,35 @@ Brain.Screen.drawRectangle(320, 60, 71, 71);
 Brain.Screen.setPenColor(green);
 Brain.Screen.setFillColor(blue);
 Brain.Screen.drawCircle(357, 97, 28);
-
-
-
-
-
-
-
-
   return 0; 
+}
+void robotDrive(int rspeed, int lspeed, float wt){
+  LF.spin(forward, lspeed, pct);
+  RF.spin(forward, rspeed, pct);
+  LB.spin(forward, lspeed, pct);
+  RB.spin(forward, rspeed, pct);
+  wait(wt, msec);
+}
+void robotStop(){
+  LF.stop(brake);
+  RF.stop(brake);
+  LB.stop(brake);
+  RB.stop(brake);
+}
+void inchDrive(float target){
+  float x = 0;
+  float error= target-x;
+  float kp = 2;
+  float speed = kp * error;
+  LF.setPosition(0, rev);
+  while(fabs (error)> 0.05){
+
+    robotDrive(speed, speed, 10);
+    x = LF.position(rev)*dia*pi*gr;
+    error = target-x;
+    speed = kp*error;
+  }
+  robotStop();
 }
 /*---------------------------------------------------------------------------*/
 
@@ -65,10 +93,8 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  // ..........................................................................
-drawWithpen();
-
-
+  // ..........................................................................hdhdhdhdhddddhhdii dhdhdhdhdhdttht',.pyf      ..
+inchDrive(10);
   // ..........................................................................
 }
 
